5aa7ae82af2d0ab105889cf6bef39778d3dd88a8
Carlos J Pacheco <Carlosj.pacheco15@gmail.com>
2022-08-10 15:55:18 -0400

    Agregando los Proyectos

---
+++ APP/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace APP
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseAll(object sender, EventArgs e)
+        {
+            foreach (Form childForm in MdiChildren)
+            {
+                childForm.Close();
+            }
+        }
+    }
+}
